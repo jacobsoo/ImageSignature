e5429c9a958bcc2299a3e7e10e7396bff3f482f5
Jacob Soo <jacobsoo@users.noreply.github.com>
2013-09-16 14:29:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <?php
-	header("Content-type: image/png");
-	Header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
-	Header('Expires: Thu, 19 Nov 1999 08:52:00 GMT');
-	Header('Pragma: no-cache');
-	$image = imagecreatefrompng("arteam-crew.png");
-	
-	if ($_SERVER["HTTP_X_FORWARDED_FOR"]) {
-		if ($_SERVER["HTTP_CLIENT_IP"]) {
-			$proxy = $_SERVER["HTTP_CLIENT_IP"];
-		} else {
-			$proxy = $_SERVER["REMOTE_ADDR"];
-		}
-		$ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
-	} else {
-		if ($_SERVER["HTTP_CLIENT_IP"]) {
-			$ip = $_SERVER["HTTP_CLIENT_IP"];
-		} else {
-			$ip = $_SERVER["REMOTE_ADDR"];
-		}
-	}
-	
-	// Simple OS Detection
-	$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
-	$os_array = Array (
-		'/windows nt 6.2/i'     =>  'Windows 8',
+    header("Content-type: image/png");
+    Header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+    Header('Expires: Thu, 19 Nov 1999 08:52:00 GMT');
+    Header('Pragma: no-cache');
+    $image = imagecreatefrompng("arteam-crew.png");
+    
+    if ($_SERVER["HTTP_X_FORWARDED_FOR"]) {
+        if ($_SERVER["HTTP_CLIENT_IP"]) {
+            $proxy = $_SERVER["HTTP_CLIENT_IP"];
+        } else {
+            $proxy = $_SERVER["REMOTE_ADDR"];
+        }
+        $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+    } else {
+        if ($_SERVER["HTTP_CLIENT_IP"]) {
+            $ip = $_SERVER["HTTP_CLIENT_IP"];
+        } else {
+            $ip = $_SERVER["REMOTE_ADDR"];
+        }
+    }
+    
+    // Simple OS Detection
+    $user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+    $os_array = Array (
+        '/windows nt 6.2/i'     =>  'Windows 8',
         '/windows nt 6.1/i'     =>  'Windows 7',
         '/windows nt 6.0/i'     =>  'Windows Vista',
         '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
@@ -45,17 +45,17 @@
         '/android/i'            =>  'Android',
         '/blackberry/i'         =>  'BlackBerry',
         '/webos/i'              =>  'Mobile'
-	);
-
+    );
+ 
     foreach ($os_array as $regex => $value) {
         if (preg_match($regex, $user_agent)) {
             $os_platform = $value;
         }
     }
-	
-	//browser type
-	$agent = $_SERVER['HTTP_USER_AGENT'];
-	// Browser type
+    
+    //browser type
+    $agent = $_SERVER['HTTP_USER_AGENT'];
+    // Browser type
     $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
     // you can add different browsers with the same way ..
     if(preg_match('/(chromium)[ \/]([\w.]+)/', $ua))
@@ -70,26 +70,28 @@
             $browser = 'msie';
     elseif(preg_match('/(mozilla)[ \/]([\w.]+)/', $ua))
             $browser = 'mozilla';
-
+ 
     preg_match('/('.$browser.')[ \/]([\w]+)/', $ua, $version);
   
-	$font_blue = imagecolorallocate($image, 225, 225, 225);
-	//($image, fontsize, rightindent, downindent, data, txtcolour)
-	$l1= "Your IP is: " . $ip;
-	$l2= "Your OS is: " . $os_platform;
-	$l3= "Your browser is " . $browser . " " . $version[2];
-	imagestring($image, 2, 12, 3, $l1, $font_blue);  
-	imagestring($image, 2, 12, 12, $l2, $font_blue); 
-	imagestring($image, 2, 12, 21, $l3, $font_blue);
-	
-	imagepng($image);
-	imagedestroy($image);
-	
+    $font_blue = imagecolorallocate($image, 225, 225, 225);
+    //($image, fontsize, rightindent, downindent, data, txtcolour)
+    $l1= "Your IP is: " . $ip;
+    $l2= "Your OS is: " . $os_platform;
+    $l3= "Your browser is " . $browser . " " . $version[2];
+    $utc_str = gmdate("M d Y H:i:s", time());
+    $gmtimenow = "Timestamp: " . $utc_str . "UTC";
+    imagestring($image, 2, 12, 3, $l1, $font_blue);  
+    imagestring($image, 2, 12, 12, $l2, $font_blue); 
+    imagestring($image, 2, 12, 21, $l3, $font_blue);
+    imagestring($image, 2, 12, 31, $gmtimenow, $font_blue);
+    
+    imagepng($image);
+    imagedestroy($image);
+    
     $host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-	$date = date('l dS \of F Y h:i:s A');
-	$somecontent = "\n$os_platform\n$ip\n$browser\n$host\n$date\n";
-	$file = fopen('test.txt', 'a+');
-	
-	fwrite($file, $somecontent); 
-	fclose($file);
+    $somecontent = "\n$os_platform\n$ip\n$browser\n$host\n$utc_str\n$ua\n";
+    $file = fopen('test.txt', 'a+');
+    
+    fwrite($file, $somecontent); 
+    fclose($file);
 ?>
